f2a760e594f22f99eb0184ab440ab9306a75e67a
mariaprimbas <primbasmaria@gmail.com>
2026-04-05 22:44:50 -0300

    Update main.py

--- main.py
+++ main.py
@@ -5,3 +5,14 @@ from src.carga_datos import cargar_datos
 from src.validacion_datos import validar_registro
 from src.metricas import calcular_metricas
 
+datos= cargar_datos("datos/datos_proyecto.csv")
+
+datos_validos= []
+
+for registro in datos:
+    if validar_registro(registro):
+        datos_validos.append(registro)
+
+resultado= calcular_metricas(datos_validos)
+
+print(resultado)
